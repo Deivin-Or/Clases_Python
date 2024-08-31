efd3859bfa5545732a0372905a1fbab25d89842f
Deivin-Or <baldeivin82@gmail.com>
2024-08-31 16:21:51 -0600

    "Actualización de contenido cmún"

---
+++ Module_Two/if_elif_else.py
@@ -0,0 +1,10 @@
+# if
+# Es usada para la toma de decisiones
+# Se utiliza para ejecutar el bloque de código si se cumplen una condición en especifico.
+
+# en este caso usamos if para imprimer guau guau si cumple y si que con el bloque de codigo 
+# si no se cumpe el requisto este no imprimra nada y seguira con el bloquye de código siguiente.
+animal = input("Dame un nombre de un animal: ")
+if animal == "perro":
+    print("guau guau")
+print("no conozco muchos animales")
